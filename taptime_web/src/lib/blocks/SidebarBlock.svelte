d75a14564e706f6ed80aacecffe7b203dd25301a
whs31 <kerbalspacefruit@gmail.com>
2026-05-07 13:16:23 +0300

    Test

---
+++ taptime_web/src/lib/blocks/SidebarBlock.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+  import type { ComponentProps } from "svelte";
+  import Logo from "$lib/icons/Logo.svelte";
+
+  let { ...restProps }: ComponentProps<typeof Sidebar.Root> = $props();
+</script>
+
+<Sidebar.Root collapsible="offcanvas" {...restProps}>
+  <Sidebar.Header>
+    <Sidebar.Menu>
+      <Sidebar.MenuItem>
+        <Sidebar.MenuButton class="data-[slot=sidebar-menu-button]:p-1.5!">
+          {#snippet child({ props })}
+            <a href="##" {...props}>
+              <Logo class="size-5!" />
+              <span class="text-base font-semibold">TapTime</span>
+            </a>
+          {/snippet}
+        </Sidebar.MenuButton>
+      </Sidebar.MenuItem>
+    </Sidebar.Menu>
+  </Sidebar.Header>
+  <!-- <Sidebar.Content>
+    <NavMain items={data.navMain} />
+    <NavDocuments items={data.documents} />
+    <NavSecondary items={data.navSecondary} class="mt-auto" />
+  </Sidebar.Content>
+  <Sidebar.Footer>
+    <NavUser user={data.user} />
+  </Sidebar.Footer> -->
+</Sidebar.Root>
--- taptime_web/src/routes/+page.svelte
+++ taptime_web/src/routes/+page.svelte
@@ -1,1 +1,24 @@
-<h1 class="text-2xl font-bold">TapTime Web</h1>
+<script lang="ts">
+  import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+  import SidebarBlock from "$lib/blocks/SidebarBlock.svelte";
+</script>
+
+<Sidebar.Provider
+  style="--sidebar-width: calc(var(--spacing) * 72); --header-height: calc(var(--spacing) * 12);"
+>
+  <SidebarBlock variant="inset" />
+  <Sidebar.Inset>
+    <!-- <SiteHeader /> -->
+    <!-- <div class="flex flex-1 flex-col">
+      <div class="@container/main flex flex-1 flex-col gap-2">
+        <div class="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+          <SectionCards />
+          <div class="px-4 lg:px-6">
+            <ChartAreaInteractive />
+          </div>
+          <DataTable {data} />
+        </div>
+      </div>
+    </div> -->
+  </Sidebar.Inset>
+</Sidebar.Provider>
