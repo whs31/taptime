081eafde93e7cda53c28715b42a8a9daf0846f93
whs31 <kerbalspacefruit@gmail.com>
2026-06-23 12:15:09 +0300

    FEAT: Redirect to the dashboard on logo click

--- taptime_web/src/lib/blocks/SidebarBlock.svelte
+++ taptime_web/src/lib/blocks/SidebarBlock.svelte
@@ -32,7 +32,7 @@
       <Sidebar.MenuItem>
         <Sidebar.MenuButton class="data-[slot=sidebar-menu-button]:p-1.5!">
           {#snippet child({ props })}
-            <a href="##" {...props}>
+            <a href="/dashboard" {...props}>
               <Logo class="size-5!" />
               <span class="text-base font-semibold">TapTime</span>
             </a>
